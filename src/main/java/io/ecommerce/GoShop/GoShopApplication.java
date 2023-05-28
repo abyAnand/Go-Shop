d73b8abe5847bbc9f026611931719d7962753638
abyAnand <abianand382@gmail.com>
2023-05-28 16:26:44 +0530

    Initial Commit

---
+++ src/main/java/io/ecommerce/GoShop/GoShopApplication.java
@@ -0,0 +1,13 @@
+package io.ecommerce.GoShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/ecommerce/GoShop/GoShopApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ecommerce.GoShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
