fe902dc50ad5a045b76ceae2d506f313c87f2128
abyAnand <abianand382@gmail.com>
2023-06-09 20:39:49 +0530

    Now security work

--- src/main/java/io/ecommerce/GoShop/Configuration/SecurityConfig.java
+++ src/main/java/io/ecommerce/GoShop/Configuration/SecurityConfig.java
@@ -1,77 +1,89 @@
 package io.ecommerce.GoShop.Configuration;
 
+
 import io.ecommerce.GoShop.service.user.UserInfoDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        return http.csrf().disable()
                 .authorizeHttpRequests()
                 .antMatchers("/static/**")
                 .permitAll()
-                .antMatchers("/", "/user/**", "/send-otp", "/verify-otp", "/register")
-                .permitAll()
-                .antMatchers("/js/register.js").permitAll()
-                .anyRequest().authenticated()
+                .antMatchers("/","/user/**","/send-otp","/verify-otp","/register")
+               .permitAll()
                 .and()
+                .authorizeHttpRequests()
+                .antMatchers("/index")
+                .authenticated().and()
                 .formLogin()
                 .loginPage("/login")
                 .permitAll()
                 .defaultSuccessUrl("/", true)
-                .successHandler(new RefererAuthenticationSuccessHandler())
                 .and()
                 .exceptionHandling()
-                .accessDeniedHandler((request, response, accessDeniedException) -> response.sendRedirect("/access-denied"))
+                .accessDeniedHandler((request, response, accessDeniedException) -> response.sendRedirect("/access-denied"))// Custom forbidden error page
                 .and()
-                .logout()
-                .logoutUrl("/logout")
-                .logoutSuccessUrl("/login?logout")
-                .invalidateHttpSession(true)
-                .deleteCookies("JSESSIONID")
-                .permitAll();
-
-        http.requestMatchers()
-                .requestMatchers(new AntPathRequestMatcher("/static/**"));
-
-        return http.build();
+                .formLogin()
+                .successHandler((request, response, authentication) -> {
+                    for (GrantedAuthority auth : authentication.getAuthorities()) {
+                        if (auth.getAuthority().equals("ROLE_ADMIN")) {
+                            response.sendRedirect("/admin/users");
+                        }
+                    }
+                })
+                .and()
+                .build();
     }
 
-    // Password encoder
+
+    //password encoder
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 
+
     @Bean
-    public AuthenticationProvider authenticationProvider() {
+    public AuthenticationProvider authenticationProvider(){
+        //giving info about who is the user details service and password encoder
+        //these info can be used to generate user details object and set it to authentication object.
         DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
         authenticationProvider.setUserDetailsService(userDetailsService());
         authenticationProvider.setPasswordEncoder(passwordEncoder());
+
         return authenticationProvider;
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
+    public UserDetailsService userDetailsService(){
         return new UserInfoDetailsService();
+    }
+
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/static/**","/templates/**","/static/js/**");
     }
 
 
